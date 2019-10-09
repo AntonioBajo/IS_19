c203821c132e5513993038aa456fa37e1ead9379
AntonioBajo <55484206+AntonioBajo@users.noreply.github.com>
2019-10-09 19:13:24 +0200

    Anular cita del paciente

---
+++ requisitos/casos_de_uso/07_anular_cita.m
@@ -0,0 +1,15 @@
+# Anula cita del paciente
+
+**ID:** 07 **Descripcion:** Se introduce el dia de la cita que se quiera anular y se queda el hueco libre.
+
+**Actores principales:** Secretario **Actores secundarios:** Paciente
+
+**Flujo principal:**
+1. El secretario debe anular la cita del paciente
+2. El secretario abre el apartado de las citas 
+3. El secretario anula la cita del paciente
+4. El sistema muestra por pantalla que la cita se ha anulado correctamente
+
+**Flujos alternativos:**
+
+4.a Si el sistema no ha podido anular la cita, mostrará un mensaje por pantalla indicando el porqué.
